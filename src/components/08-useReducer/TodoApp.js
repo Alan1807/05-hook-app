2dafb219ee2c7f53e5a0342cc72f10da33a9184b
Alan <alannmv@hotmail.com>
2021-06-29 21:19:13 -0600

    useReducer - Terminado

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,29 +1,88 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
 import './styles.css';
 
-const initialState = [{
-    id: new Date().getTime(),
-    desc: 'Aprender React',
-    done: false
-}];
+const init = () => {
+
+    // Si el local storage está vacío regresará un arreglo vacío,
+    // de lo contrario regresará todos los 'todos' que estén en el local storage
+    return JSON.parse( localStorage.getItem('todos') ) || [];
+
+    // return [{
+    //     id: new Date().getTime(),
+    //     desc: 'Aprender React',
+    //     done: false
+    // }];
+}
 
 export const TodoApp = () => {
 
-    const [ todos ] = useReducer(todoReducer, initialState);
-    console.log(todos);
+    // Crea reducer con el estado inicial vacío y la función init
+    // que leerá el local storage...si está vacío retorna un arreglo vacío,
+    // de lo contrario retornará los 'todos' y será el nuevo estado
+    // todos = STATE
+    // dispatch = FUNCION QUE RECIBIRÁ LOS ACTIONS
+    const [ todos, dispatch ] = useReducer(todoReducer, [], init);
+
+    // Effect que se ejecutará cada vez que 'todos' cambie. Almacenará
+    // los 'todos' en el local storage
+    // (que en este punto ya se agrega el todo capturado en pantalla al listado)
+    useEffect(() => {
+        
+        localStorage.setItem( 'todos', JSON.stringify( todos ) );
+        
+    }, [ todos ]);
+
+    const handleDelete = ( todoId ) => {
+
+        const action = {
+            type: 'delete', 
+            payload: todoId
+        }
+
+        dispatch( action );
+
+    }
+
+    const handleToggle = ( todoId ) => {
+
+        dispatch({
+            type: 'toggle',
+            payload: todoId
+        });
+
+    }
+
+    const handleAddTodo = ( newTodo ) => {
+
+        dispatch({
+            type: 'add',
+            payload: newTodo
+        });
+
+    }
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({ todos.length })</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+
+                    <TodoList todos={ todos } handleDelete={ handleDelete } handleToggle={ handleToggle } />
+
+                </div>
+                
+                <div className="col">
+                    
+                    <TodoAdd handleAddTodo={ handleAddTodo } />
+
+                </div>
+            </div>            
 
         </div>
     )
